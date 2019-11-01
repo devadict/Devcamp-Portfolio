47fce958b31bd34f5423c35249b35ebae74ae61a
Hakim Sy <hakimsy17@gmail.com>
2019-11-01 04:47:11 +0100

    Implemented destroyed action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,4 +37,16 @@ def new
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
+ def destroy
+  #Perform the lookup
+  @portfolio_item = Portfolio.find(params[:id])
+  #Destroy the record
+
+    @portfolio_item.destroy
+  # Redirect  
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Post was successfully destroyed.' }
+     
+    end
+  end
 end
