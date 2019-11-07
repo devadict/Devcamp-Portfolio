4bcb45decdf4574506065db81ebf23e34142f590
Hakim Sy <hakimsy17@gmail.com>
2019-11-07 03:57:26 +0100

    Customized registration, login, and logout routes

--- db/migrate/20191107021016_add_trackable_to_devise.rb
+++ db/migrate/20191107021016_add_trackable_to_devise.rb
@@ -6,6 +6,7 @@ class AddTrackableToDevise < ActiveRecord::Migration[6.0]
     add_column :users, :current_sign_in_ip, :inet
     add_column :users, :last_sign_in_ip, :inet
   end
+  
   def down
   	remove_columns :users, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
   end
