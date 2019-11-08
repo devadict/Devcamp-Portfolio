39066d0a4f3504bcb5420635570dc647297445be
Hakim Sy <hakimsy17@gmail.com>
2019-11-08 04:50:08 +0100

    Completed autentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+         def first_name
+         	
+         end
+         def last_name
+         	
+         end
 end
